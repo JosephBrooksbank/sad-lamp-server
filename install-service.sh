9668ab0f373eeedc22551cd5a370144b8666dd95
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2024-01-13 12:35:39 -0600

    Use http and don't move service

--- install-service.sh
+++ install-service.sh
@@ -9,7 +9,7 @@ CURRENT_DIR="$(pwd)"
 # Check if the service file exists in the current directory
 if [ -e "$CURRENT_DIR/$SERVICE_FILE" ]; then
     # Move the service file to the correct systemd directory
-    sudo mv "$CURRENT_DIR/$SERVICE_FILE" "/etc/systemd/system/$SERVICE_FILE"
+    sudo cp "$CURRENT_DIR/$SERVICE_FILE" "/etc/systemd/system/$SERVICE_FILE"
 
     # Add WorkingDirectory parameter under [Service] in the service file
     sudo sed -i "/\[Service\]/a WorkingDirectory=$CURRENT_DIR" "/etc/systemd/system/$SERVICE_FILE"
